68e27c9b5315fc3a1c95b84080d9dcb28a56aeb7
Level222 <124219660+Level222@users.noreply.github.com>
2024-09-23 10:43:47 +0900

    Add a newline at the end of package-data-list.json

--- scripts/create-package-data-list/index.js
+++ scripts/create-package-data-list/index.js
@@ -5,4 +5,4 @@ import packageInfoList from './package-info-list.js';
 
 const packageDataList = await Promise.all(packageInfoList.map(createPackageData));
 const outPath = path.resolve(import.meta.dirname, '../../data/package-data-list.json');
-await fs.writeFile(outPath, JSON.stringify(packageDataList));
+await fs.writeFile(outPath, `${JSON.stringify(packageDataList)}\n`);
